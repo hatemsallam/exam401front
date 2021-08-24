dca8e750a57d5f5fcef86b916b4686ba94f19ca5
hatemsallam <hatemsallam94@hotmail.com>
2021-08-24 14:00:18 +0300

    import modal and form

--- src/components/FavFlowers.js
+++ src/components/FavFlowers.js
@@ -2,7 +2,7 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import axios from 'axios';
 import { withAuth0 } from '@auth0/auth0-react';
-import { Card, Button } from 'react-bootstrap'
+import { Card, Button , Modal , Form } from 'react-bootstrap'
 
 
 
